94cb80c1b22b9d6c6497a72fc24fcc0b5b435e2b
rogerskuma <107814710+rogerskuma@users.noreply.github.com>
2022-12-28 16:47:34 -0600

    new file:   controllers/LoginController.php 	modified:   public/index.php

---
+++ controllers/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Controllers;
+
+class LoginController {
+    public static function login() {
+        echo "Desde login";
+    }
+
+    public static function logout() {
+        echo "Desde logout";
+    }
+
+    public static function olvide() {
+        echo "olvide";
+    }
+
+    public static function recuperar() {
+        echo "recuperar";
+    }
+
+    public static function crear() {
+        echo "crear cuenta";
+    }
+}
