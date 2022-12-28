80ffa69fd547b91b93177d14556f1611725207cf
rogerskuma <107814710+rogerskuma@users.noreply.github.com>
2022-12-28 17:18:51 -0600

    modified:   public/build/css/app.css 	modified:   public/build/css/app.css.map 	modified:   src/scss/app.scss 	new file:   src/scss/layout/_UI.scss 	new file:   src/scss/layout/_index.scss 	modified:   views/layout.php

--- views/layout.php
+++ views/layout.php
@@ -9,7 +9,14 @@
 </head>
 <body>
 
-    <?php echo $contenido; ?>
-            
+    <div class="contendor-app">
+        <div class="imagen"></div>
+        <div class="app">
+        <?php echo $contenido; ?>      
+        </div>
+    </div>
+
+
+                
 </body>
 </html>
