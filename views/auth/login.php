26ca79764de367ef1b021dddd57602e1e0cb99e8
rogerskuma <107814710+rogerskuma@users.noreply.github.com>
2022-12-28 18:09:16 -0600

    modified:   public/build/css/app.css 	modified:   public/build/css/app.css.map 	modified:   src/scss/app.scss 	new file:   src/scss/componentes/_formulario.scss 	new file:   src/scss/componentes/_index.scss 	modified:   views/auth/login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,33 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-página"> Inicia sesión con tus datos</p>
+
+<form class="formulario" method="POST" action="/">
+
+    <div class="campo">
+
+        <label for="email">Email</label>
+        <input
+            type="email"
+            id="email"
+            placeholder="tu email"
+            name="email"
+        />
+    </div>
+<div class="campo">
+
+        <label for="password">Password</label>
+        <input
+            type="password"
+            id="password"
+            placeholder="tu password"
+            name="password"
+        />       
+    </div>
+    <input type="submit" class="boton"  value="Iniciar Sesión"/>
+</form>
+
+
+<div class="acciones">
+    <a href="/crear-cta">Crea Tu Cuenta</a>
+    <a href="/olvide">Recuperación de Password</a>
+</div> 
