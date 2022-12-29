4b54b477638b13d3d182fe393d8642618b5d0e35
rogerskuma <107814710+rogerskuma@users.noreply.github.com>
2022-12-29 16:45:52 -0600

    new file:   classes/Email.php 	modified:   controllers/LoginController.php 	modified:   composer.json 	modified:   composer.lock 	modified:   models/Usuario.php 	modified:   public/build/css/app.css 	modified:   public/build/css/app.css.map 	modified:   src/scss/componentes/_index.scss 	modified:   views/auth/crear-cta.php 	src/scss/componentes/_alertas.scss 	views/templates/

---
+++ classes/Email.php
@@ -0,0 +1,21 @@
+<?php
+namespace Classes;
+
+class Email {
+
+    public $email;
+    public $nombre;
+    public $token;
+
+    public function __construct($email, $nombre, $token) 
+    {
+            $this->email = $email;
+            $this->nombre = $nombre;
+            $this->token = $token;
+    }
+
+    public function enviarConfirmacion() {
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+    }
+}
--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Controllers;
 
+use Classes\Email;
 use Model\Usuario;
 use MVC\Router;
 
@@ -26,16 +27,45 @@ class LoginController {
 
     public static function crear(Router $router) {
         $usuario = new Usuario;
+
+        //Alertas vacias
+        $alertas = [];
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             
             $usuario->sincronizar($_POST);
-
+            $alertas = $usuario->validarNuevaCuenta();
             
+            //Revisar que alerta este vacio
+            if(empty($alertas)) {
+                //Verificar que el usuario no este registrado
+                $resultado = $usuario->existeUsuario();
+
+                if($resultado->num_rows) {
+                    $alertas = Usuario::getAlertas();
+                } else {
+                    //Hashear el password
+                    $usuario->hashPassword(); 
+                    //"No esta registrado"
+                    
+                    //Generar un Token único
+                    $usuario->crearToken();
+
+                    //Enviar el email
+
+                    $email = new Email($usuario->nombre, $usuario->email, $usuario->token);
+
+                    $email->enviarConfirmacion();
+
+                    debuguear($email);
+                    debuguear($usuario);
+                }
+            }     
         }
 
 
         $router->render('/auth/crear-cta' , [
-            'usuario' => $usuario
+            'usuario' => $usuario,
+            'alertas' => $alertas
         ]); 
     }
 }
