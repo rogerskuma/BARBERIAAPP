6647c0ee83ce124bc55ac5015a69d91ddb8b57a6
rogerskuma <107814710+rogerskuma@users.noreply.github.com>
2023-02-14 08:50:06 -0600

    modified:   Router.php

--- Router.php
+++ Router.php
@@ -20,7 +20,7 @@ class Router
     public function comprobarRutas()
     {
 
-        $currentUrl = $_SERVER['REQUEST_URI'] === ' ' ? '/ ' : $_SERVER['REQUEST_URI'];
+        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/ ' : $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
